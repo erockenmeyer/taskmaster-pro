3b54d8bc328acb3afa5e4c717f06268f566e2378
erockenmeyer <erikrockenmeyer@gmail.com>
2022-03-03 15:11:50 -0600

    added sortability and draggable deletion

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,6 +166,54 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  update: function(event) {
+    //array to store the task data in
+    var tempArr = []
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      //add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    
+    // trim down list's id to match obj property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks obj and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+// turn bottom bar into trash to delete one item by dragging
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
